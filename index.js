781ca8f7f0e43909810336a12c817f9d13dfc31b
Vasu Jain <vasu.jain@live.in>
2016-05-19 00:46:35 -0700

    Changes in controller

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ if (process.env.TOKEN || process.env.SLACK_TOKEN) {
 //} else if (process.env.CLIENT_ID && process.env.CLIENT_SECRET && process.env.PORT) {
     //Treat this as an app
     var app = require('./lib/apps');
-    var controller = app.configure(process.env.PORT, process.env.CLIENT_ID, process.env.CLIENT_SECRET, config, onInstallation);
+//    var controller = app.configure(process.env.PORT, process.env.CLIENT_ID, process.env.CLIENT_SECRET, config, onInstallation);
 //} else {
 //    console.log('Error: If this is a custom integration, please specify TOKEN in the environment. If this is an app, please specify CLIENTID, CLIENTSECRET, and PORT in the environment');
 //    process.exit(1);
